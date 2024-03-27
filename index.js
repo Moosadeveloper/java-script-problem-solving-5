a3c9b75ec010dc483e905a3440e2037cb23253c6
Moosadeveloper <janmoosa77@gmail.com>
2024-03-27 19:41:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var num = 20 % 6;
+// value is 2.
+var largeNum = 22;
+largeNum =1000*2000;
+// Answer is 2000000.
+
+var moosa = 100;
+var zakeer =10-moosa;
+// Answer is -90.
+
+var ahsan =35%2;
+var hassan = 1/100;
+// Answer is 0.01.
+
+var assan =5*2;
+alert(assan);
+
